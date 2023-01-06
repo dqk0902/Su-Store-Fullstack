55a5c337f1bdf1a2164bc2afeb8b410ec5acb4ee
unknown <duongquockhang.1205@gmail.com>
2023-01-06 20:14:28 +0200

    Handeling hamburger menu

--- src/pages/RootPage/Root.tsx
+++ src/pages/RootPage/Root.tsx
@@ -5,45 +5,40 @@ import "../../styles/root.css";
 const Root = () => {
   return (
     <div>
-      <nav className="navbar">
-        <div className="navbar-container">
-          <a href="./" id="navbar-logo">
-            Su's Store
-          </a>
-          <div className="navbar-toggle" id="mobile-menu">
-            <span className="bar"></span>
-            <span className="bar"></span>
-            <span className="bar"></span>
-          </div>
-          <ul className="navbar-menu">
-            <li className="navbar-itme">
-              <Link to="" className="navbar-links">
-                Home
-              </Link>
-            </li>
-            <li className="navbar-itme">
-              <Link to="products" className="navbar-links">
-                Products
-              </Link>
-            </li>
-            <li className="navbar-itme">
-              <Link to="profile" className="navbar-links">
-                Profile
-              </Link>
-            </li>
-            <li className="navbar-itme">
-              <Link to="cart" className="navbar-links">
-                Cart
-              </Link>
-            </li>
-            <li className="navbar-itme">
-              <Link to="login" className="navbar-links">
-                Login
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <section className="top-nav">
+        <div>Su's Store</div>
+        <input id="menu-toggle" type="checkbox" />
+        <label className="menu-button-container" htmlFor="menu-toggle">
+          <div className="menu-button"></div>
+        </label>
+        <ul className="menu">
+          <li className="navbar-itme">
+            <Link to="" className="navbar-links">
+              Home
+            </Link>
+          </li>
+          <li className="navbar-itme">
+            <Link to="products" className="navbar-links">
+              Products
+            </Link>
+          </li>
+          <li className="navbar-itme">
+            <Link to="profile" className="navbar-links">
+              Profile
+            </Link>
+          </li>
+          <li className="navbar-itme">
+            <Link to="cart" className="navbar-links">
+              Cart
+            </Link>
+          </li>
+          <li className="navbar-itme">
+            <Link to="login" className="navbar-links">
+              Login
+            </Link>
+          </li>
+        </ul>
+      </section>
       <Outlet />
     </div>
   );
